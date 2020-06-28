927cb1abae7981f105c4b8e2c9f932ba73438789
Prajakt <khawse.prajakt@gmail.com>
2020-06-28 11:59:57 -0500

    add logic to the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'untitled';
+
+  backendUrl = 'http://localhost:8080/getAlNumPhoneNumbers';
+  title = 'demo-project';
+
+  constructor(private http: HttpClient) {}
+  result: any;
+  results: any;
+  phoneNumber: any;
+  totalResults: any;
+  count: number;
+  // tslint:disable-next-line:typedef
+  getAlNumNumbers(phoneNumber) {
+
+    this.http.get(this.backendUrl.concat('/').concat(phoneNumber).concat('/totalResults')).subscribe(data => {
+      this.totalResults = data;
+    });
+    this.count = 0;
+    this.http.get(this.backendUrl.concat('/').concat(phoneNumber).concat('?page=0&size=10')).subscribe(data => {
+      this.results = data.toString().split(',');
+      this.result = '';
+      // tslint:disable-next-line:prefer-for-of
+      for (let i = 0; i < this.results.length; i++){
+        this.result = this.result.concat(this.results[i]);
+        this.result = this.result.concat('\n');
+      }
+    });
+  }
+  // tslint:disable-next-line:typedef
+  getNextSet(phoneNumber) {
+    this.count = this.count + 1;
+    this.http.get(this.backendUrl.concat('/').concat(phoneNumber).concat('?page=').concat(String(this.count)).concat('&size=10')).subscribe(data => {
+      this.results = data.toString().split(',');
+      console.log(this.results);
+      this.result = '';
+      // tslint:disable-next-line:prefer-for-of
+      for (let i = 0; i < this.results.length; i++){
+        this.result = this.result.concat(this.results[i]);
+        this.result = this.result.concat('\n');
+      }
+    });
+
+  }
 }
